bb4bf184b5a58b218c1af19931cace36f43d4d4b
Siow Meng Low <low.siowmeng@amaris.ai>
2019-03-01 04:34:58 +0800

    Added function

--- classes.py
+++ classes.py
@@ -6,6 +6,8 @@ Created on Fri Mar  1 02:10:18 2019
 @author: lowsiowmeng
 """
 
+from itertools import combinations
+
 # Input Class: List of Photos
 class photo():
     
@@ -52,4 +54,52 @@ def calcSlideshowScores(listSlides):
         totalScore += calcTransScores(prevSlide, currSlide)
     
     return totalScore
+
+def getVCombi(vPhotos):
+    vCombi = combinations(range(len(vPhotos)), 2)
     
+    vSlidesCandidates = [None] * len(vPhotos)
+    vSlidesBestTags = [0] * len(vPhotos)
+    vSlidesBestPairs = [-1] * len(vPhotos)
+    for idx1, idx2 in vCombi:
+        currSlide = slide('V', [vPhotos[idx1], vPhotos[idx2]])
+        if len(currSlide.tags) > vSlidesBestTags[idx1]:
+            vSlidesCandidates[idx1] = currSlide
+            vSlidesBestTags[idx1] = len(currSlide.tags)
+            vSlidesBestPairs[idx1] = idx2
+        if len(currSlide.tags) > vSlidesBestTags[idx2]:
+            vSlidesCandidates[idx2] = currSlide
+            vSlidesBestTags[idx2] = len(currSlide.tags)
+            vSlidesBestPairs[idx2] = idx1
+    
+    return vSlidesCandidates, vSlidesBestTags, vSlidesBestPairs
+
+def findCombi(listPhotos):
+#    vPhotoID = []
+#    vPhotoTags = []
+    vPhotos = []
+#    hPhotoID = []
+#    hPhotoTags = []
+    hPhotos = []
+    
+    for photo in listPhotos:
+        if photo.orient == 'H':
+#            hPhotoID.append(photo.photoID)
+#            hPhotoTags.append(photo.tags)
+            hPhotos.append(photo)
+        elif photo.orient == 'V':
+#            vPhotoID.append(photo.photoID)
+#            vPhotoTags.append(photo.tags)
+            vPhotos.append(photo)
+    
+    vCombi = combinations(range(len(vPhotos)), 2)
+    
+    vSlidesCandidates = []
+    for idx1, idx2 in vCombi:
+        vSlidesCandidates.append(slide('V', [vPhotos[idx1], vPhotos[idx2]]))
+    
+    hSlidesCandidates = []
+    for photo in hPhotos:
+        hSlidesCandidates.append(slide('H', [photo]))
+    
+    return hSlidesCandidates, vSlidesCandidates
